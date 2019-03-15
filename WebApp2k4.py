6d670f1ba406fb4704934b4cc41d38c14a8ae83d
Sohil <spramij@mun.ca>
2019-03-15 11:58:36 -0230

    Basically just like the spramij/cs2006, but cs2004

---
+++ WebApp2k4.py
@@ -0,0 +1,28 @@
+from flask import *
+import os, pypandoc, random
+
+
+app = Flask(__name__)
+
+navbar = []
+for file in os.listdir("resources"):
+    navbar.append(str(file)[:-3])
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", navbar=navbar)
+
+
+@app.route("/<chapter>")
+def give_me(chapter):
+    file_to_compile = "resources/"+chapter+".md"
+    this = pypandoc.convert_file(file_to_compile, 'html')
+
+    return render_template("chapters.html", navbar=navbar, chapter=this)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
