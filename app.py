d6d3d8f9b6208e669a96ec3da60aacb49862e862
Sohil <spramij@mun.ca>
2019-04-05 18:08:36 -0230

    Added persistent dark mode

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import os, pypandoc, random
 
 app = Flask(__name__)
 
+global inverted
 global navbar
+
+inverted = False
 def update_navbar():
     return [str(file)[:-3] for file in os.listdir("resources")]
 
 @app.route("/")
 def index():
-    return render_template("index.min.html", navbar=update_navbar())
+    return render_template("index.min.html", navbar=update_navbar(), colors=inverted)
 
 @app.route("/<chapter>")
 def give_me(chapter):
@@ -18,12 +21,19 @@ def give_me(chapter):
         file_to_compile = "resources/"+chapter+".md"
         this = pypandoc.convert_file(file_to_compile, 'html')
     except :
-        return render_template('error.min.html',  navbar=update_navbar())
-    return render_template("chapters.min.html", navbar=update_navbar(), chapter=this)
+        return render_template('error.min.html',  navbar=update_navbar(), colors=inverted)
+    return render_template("chapters.min.html", navbar=update_navbar(), chapter=this, colors=inverted)
+
+@app.route("/_invert")
+def invert_colors():
+    global inverted
+    inverted ^= 1
+    return redirect('/')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-        return render_template('error.min.html', navbar=update_navbar())
+        return render_template('error.min.html', navbar=update_navbar(), colors=inverted)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
